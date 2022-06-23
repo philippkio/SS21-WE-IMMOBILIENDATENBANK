e418db0947a8eefefe0c08a44652a7237020c7f4
philippkionke <vuztyM-comhy0-fywcos>
2022-06-23 22:56:27 +0200

    abfrage test der felder

--- Immobiliendatenbank/src/main/java/net/lehre_online/bw/MbSuchmaske.java
+++ Immobiliendatenbank/src/main/java/net/lehre_online/bw/MbSuchmaske.java
@@ -1,30 +1,44 @@
 package net.lehre_online.bw;
 
+import static java.lang.System.out;
+
 import java.io.Serializable;
 
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.event.ActionEvent;
 import jakarta.inject.Named;
 
-@Named
-@SessionScoped
-public class MbSuchmaske implements Serializable  {
 
-	private static final long serialVersionUID = 1L;
+@Named 
+@jakarta.faces.bean.RequestScoped
+public class MbSuchmaske {
 
-	private String s = "Stadt";
+    private String immobilienart;
+    private String city;
+    private String result;
 
-	
-	
-	
-	
-	public String getS() { return s; }
-	  public void setS( String s ) { this.s = s; }
-	
-	
-	
+    public void submit() {
+        result = "Submitted values: " + immobilienart + ", " + city;
+        System.out.println(result);
+    }
 
-	
-	
-	
+    public String getText() {
+        return immobilienart;
+    }
+
+    public void setText(String text) {
+        this.immobilienart = text;
+    }
+
+    public String getChoice() {
+        return city;
+    }
+
+    public void setChoice(String choice) {
+        this.city = choice;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
